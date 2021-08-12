1e9355bb288dc3b03ad2378fbb5ca799936f42f5
Stefan Niedermann <info@niedermann.it>
2021-08-12 22:33:54 +0200

    Refactor hint messages to enum

--- src/app/analyzer/analyzer.component.ts
+++ src/app/analyzer/analyzer.component.ts
@@ -22,7 +22,7 @@ export class AnalyzerComponent implements AfterViewInit {
   private readonly youAssignedCheckbox = this.form.get('youAssigned');
   private readonly someoneElseAssignedCheckbox = this.form.get('someoneElseAssigned');
 
-  explanation$: Observable<string> = EMPTY;
+  explanation$: Observable<Hint> = EMPTY;
   cardIsVisible$: Observable<boolean> = EMPTY;
   icon$: Observable<string> = EMPTY;
 
@@ -56,40 +56,48 @@ export class AnalyzerComponent implements AfterViewInit {
      * Card is in a board which is also shared with others AND (card is assigned to you OR (card has a due date AND nobody is assigned to the card))
      */
     this.explanation$ = values$.pipe(map(next => {
-      if(next.isSharedBoard && next.hasDueDate && next.youAssigned && next.someoneElseAssigned) {
-        return 'Your card should be shown.';
-      } else if(!next.isSharedBoard && next.hasDueDate && next.youAssigned && next.someoneElseAssigned) {
-        return 'Your card should be shown.';
-      } else if(next.isSharedBoard && !next.hasDueDate && next.youAssigned && next.someoneElseAssigned) {
-        return 'Your card should be shown.';
-      } else if(next.isSharedBoard && next.hasDueDate && !next.youAssigned && next.someoneElseAssigned) {
-        return 'Assign the card to you or remove all other assignees from the card.'
-      } else if(next.isSharedBoard && next.hasDueDate && next.youAssigned && !next.someoneElseAssigned) {
-        return 'Your card should be shown.';
-      } else if(!next.isSharedBoard && !next.hasDueDate && next.youAssigned && next.someoneElseAssigned) {
-        return 'Set a due date to the card.';
-      } else if(!next.isSharedBoard && next.hasDueDate && !next.youAssigned && next.someoneElseAssigned) {
-        return 'Your card should be shown.';
-      } else if(!next.isSharedBoard && next.hasDueDate && next.youAssigned && !next.someoneElseAssigned) {
-        return 'Your card should be shown.';
-      } else if(next.isSharedBoard && !next.hasDueDate && !next.youAssigned && next.someoneElseAssigned) {
-        return 'Assign the card to you or set a due date and remove the other assignees.';
-      } else if(next.isSharedBoard && !next.hasDueDate && next.youAssigned && !next.someoneElseAssigned) {
-        return 'Your card should be shown.';
-      } else if(next.isSharedBoard && next.hasDueDate && !next.youAssigned && !next.someoneElseAssigned) {
-        return 'Your card should be shown.';
-      } else if(!next.isSharedBoard && !next.hasDueDate && !next.youAssigned && next.someoneElseAssigned) {
-        return 'Set a due date to the card.';
-      } else if(!next.isSharedBoard && !next.hasDueDate && next.youAssigned && !next.someoneElseAssigned) {
-        return 'Set a due date to the card.';
-      } else if(!next.isSharedBoard && next.hasDueDate && !next.youAssigned && !next.someoneElseAssigned) {
-        return 'Your card should be shown.';
-      } else if(next.isSharedBoard && !next.hasDueDate && !next.youAssigned && !next.someoneElseAssigned) {
-        return 'Assign the card to you or set a due date.';
-      } else if(!next.isSharedBoard && !next.hasDueDate && !next.youAssigned && !next.someoneElseAssigned) {
-        return 'Set a due date.';
+      if (next.isSharedBoard && next.hasDueDate && next.youAssigned && next.someoneElseAssigned) {
+        return Hint.CARD_SHOULD_BE_SHOWN;
+      } else if (!next.isSharedBoard && next.hasDueDate && next.youAssigned && next.someoneElseAssigned) {
+        return Hint.CARD_SHOULD_BE_SHOWN;
+      } else if (next.isSharedBoard && !next.hasDueDate && next.youAssigned && next.someoneElseAssigned) {
+        return Hint.CARD_SHOULD_BE_SHOWN;
+      } else if (next.isSharedBoard && next.hasDueDate && !next.youAssigned && next.someoneElseAssigned) {
+        return Hint.ASSIGN_TO_YOU_OR_REMOVE_OTHERS;
+      } else if (next.isSharedBoard && next.hasDueDate && next.youAssigned && !next.someoneElseAssigned) {
+        return Hint.CARD_SHOULD_BE_SHOWN;
+      } else if (!next.isSharedBoard && !next.hasDueDate && next.youAssigned && next.someoneElseAssigned) {
+        return Hint.SET_DUE_DATE;
+      } else if (!next.isSharedBoard && next.hasDueDate && !next.youAssigned && next.someoneElseAssigned) {
+        return Hint.CARD_SHOULD_BE_SHOWN;
+      } else if (!next.isSharedBoard && next.hasDueDate && next.youAssigned && !next.someoneElseAssigned) {
+        return Hint.CARD_SHOULD_BE_SHOWN;
+      } else if (next.isSharedBoard && !next.hasDueDate && !next.youAssigned && next.someoneElseAssigned) {
+        return Hint.ASSIGN_TO_YOU_OR_SET_DUE_DATE_AND_REMOVE_OTHERS;
+      } else if (next.isSharedBoard && !next.hasDueDate && next.youAssigned && !next.someoneElseAssigned) {
+        return Hint.CARD_SHOULD_BE_SHOWN;
+      } else if (next.isSharedBoard && next.hasDueDate && !next.youAssigned && !next.someoneElseAssigned) {
+        return Hint.CARD_SHOULD_BE_SHOWN;
+      } else if (!next.isSharedBoard && !next.hasDueDate && !next.youAssigned && next.someoneElseAssigned) {
+        return Hint.SET_DUE_DATE;
+      } else if (!next.isSharedBoard && !next.hasDueDate && next.youAssigned && !next.someoneElseAssigned) {
+        return Hint.SET_DUE_DATE;
+      } else if (!next.isSharedBoard && next.hasDueDate && !next.youAssigned && !next.someoneElseAssigned) {
+        return Hint.CARD_SHOULD_BE_SHOWN;
+      } else if (next.isSharedBoard && !next.hasDueDate && !next.youAssigned && !next.someoneElseAssigned) {
+        return Hint.ASSIGN_CARD_OR_SET_DUE_DATE;
+      } else if (!next.isSharedBoard && !next.hasDueDate && !next.youAssigned && !next.someoneElseAssigned) {
+        return Hint.SET_DUE_DATE;
       }
-      return '';
+      throw new Error(`Invalid state: ${JSON.stringify(next)}`);
     }));
   }
+}
+
+enum Hint {
+  CARD_SHOULD_BE_SHOWN = 'Your card should be shown.',
+  SET_DUE_DATE = 'Set a due date to the card',
+  ASSIGN_CARD_OR_SET_DUE_DATE = 'Assign the card to you or set a due date.',
+  ASSIGN_TO_YOU_OR_REMOVE_OTHERS = 'Assign the card to you or remove all other assignees from the card.',
+  ASSIGN_TO_YOU_OR_SET_DUE_DATE_AND_REMOVE_OTHERS = 'Assign the card to you or set a due date and remove the other assignees.'
 }
